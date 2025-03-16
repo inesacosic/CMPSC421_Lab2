a4f42b5aa8b3a7001ed33a25fdde5240e3ce25b0
Inesa <ixc5135@psu.edu>
2025-03-15 22:21:37 -0400

    set up swagger and added routes for api calls

--- Services/index.js
+++ Services/index.js
@@ -1,43 +1,62 @@
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const swaggerJSDoc = require('swagger-jsdoc')
 const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
+/////////////////////////////////////////////////////////////
 const {connectToDB, closeDBConnection} = require('./database')
+const createOrder = require('./createOrder');
+const deleteOrder = require('./deleteOrder');
+const modifyOrder = require('./modifyOrder');
+const deleteCustomer = require('./deleteCustomer');
+const createCustomer = require('./createCustomer');
+/////////////////////////////////////////////////////////////
 
-//app.use(bodyParser.json)
+
+app.use(bodyParser.json());
+
+   // Swagger definition
+   const swaggerOptions = {
+    swaggerDefinition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'My API',
+            version: '1.0.0',
+            description: 'API documentation using Swagger',
+        },
+        servers: [
+            {
+                url: `http://localhost:${port}`,
+            },
+        ],
+   components: {
+     securitySchemes: {
+         bearerAuth: {
+             type: 'http',
+             scheme: 'bearer',
+             bearerFormat: 'JWT', 
+         },
+     },
+ },
+    },
+    apis: ['./*.js'], // Path to your API docs
+};
+
+const swaggerDocs = swaggerJSDoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
 })
 
-async function createCustomer(req, res) {
-     // get customer data from the request
-     const customerinfo = req.body;
-     // log data for debugging
-     console.log("Customer info:", customerinfo);
-     res.end()
-     try{
-         const db = await connectToDB();
- 
-         const result = await db.collection('CustomerInfo').insertOne(...customerinfo);
- 
-         res.end(201).json({
-             message: 'Customer inserted successfully',
- 
-         })
-     }catch(error){
-         console.log('Error: ', error);
-         res.end(500).json({
-             error: 'Error inserting customer'
-         })
-     }finally{
-         // close database connection
-         await closeDBConnection();
-     }
- 
-}
+
 // Routes
-app.put('/api/createcustomer', createCustomer);
+app.post('/api/createcustomer', createCustomer);
+app.post('/api/createOrder', createOrder);
+app.delete('/api/deleteOrder', deleteOrder);
+app.put('/api/modifyOrder', modifyOrder);
+app.delete('/api/deleteCustomer', deleteCustomer);
 
 // Routes
 app.get('/', (req, res) => {
